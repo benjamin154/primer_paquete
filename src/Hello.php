cbf64019a9a1d307e212abd189908d58beadd48c
benjamin154 <30879990+benjamin154@users.noreply.github.com>
2019-03-27 16:26:58 -0700

    first commit

---
+++ src/Hello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maste
+ * Date: 27/03/2019
+ * Time: 15:35
+ */
+namespace benjamin154\hello;
+
+class Hello
+{
+    public function sayHello($message)
+    {
+        echo "Hello $message\n";
+    }
+}
